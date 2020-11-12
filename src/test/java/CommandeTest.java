6637333007ed8c45fd5f14020c134a1919b07e89
William <william.andrieu@edu.itescia.fr>
2020-11-12 12:03:44 +0100

    Ajout du design pattern Memento pour l'historique d'etat des commandes
    Les tests verifient que les etats sont bien mis a jour apres les actions undo/redo et qu'un undo a vide ne plante pas

---
+++ src/test/java/CommandeTest.java
@@ -0,0 +1,56 @@
+import org.example.models.Commande;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommandeTest {
+
+    private Commande commande;
+
+    @Before
+    public void before(){
+        commande = new Commande();
+    }
+
+    @Test
+    public void updateCommandStateUpdateHistory(){
+
+        commande.setState(Commande.State.ENCOURS);
+        commande.getState();
+        Assert.assertEquals(
+                Commande.State.ENCOURS,
+                commande.getCommandHistory().getCurrentCommandState().getState());
+    }
+
+    @Test
+    public void verifyEmptyUndo(){
+       commande.getCommandHistory().undo();
+    }
+
+
+    @Test
+    public void updateCommandStateThenUndo(){
+
+        commande.setState(Commande.State.ENCOURS);
+
+        commande.restore(commande.getCommandHistory().undo());
+
+        Assert.assertEquals(Commande.State.NOUVEAU, commande.getState());
+    }
+
+
+    @Test
+    public void sequenceUndoRedo(){
+
+        commande.setState(Commande.State.ENCOURS);
+
+        commande.restore(commande.getCommandHistory().undo());
+
+        commande.restore(commande.getCommandHistory().redo());
+
+        Assert.assertEquals(Commande.State.ENCOURS, commande.getState());
+    }
+
+
+
+}
